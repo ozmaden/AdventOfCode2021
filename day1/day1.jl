420a178bcb3cd5f11b1631edfd7325c62176921f
Can Ozmaden <can.oezmaden@rwth-aachen.de>
2021-12-02 10:47:43 +0100

    Solve second part of day one

--- day1/day1.jl
+++ day1/day1.jl
@@ -1,3 +1,18 @@
+using DSP
+
 depths = parse.(Int, readlines("day1/input"))
+# depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+##  PART ONE
+#   count increasing depths
+
 num_of_increasing_depths = length(findall(x->x>0, diff(depths)))
-println("There are $(num_of_increasing_depths) measurements that are larger than the previous measurement")
+println("Part One: There are $(num_of_increasing_depths) measurements that are larger than the previous measurement")
+
+##  PART TWO
+#   Averaging window length = 3
+window_length = 3
+window = ones(Int, window_length)
+moving_averaged_depths = conv(window, depths)[window_length:end-(window_length-1)] # cut the ramps at the beginning and end
+num_of_increasing_depths = length(findall(x->x>0, diff(moving_averaged_depths)))
+println("Part Two: There are $(num_of_increasing_depths) measurements that are larger than the previous measurement")
